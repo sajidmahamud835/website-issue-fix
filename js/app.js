72f134e52066f049e46d9a50f4b6fa0041651db8
sajidmahamud835 <sajidmahamud835@gmail.com>
2021-09-14 20:22:48 +0600

    ratting added, fontawsome cdn added

--- js/app.js
+++ js/app.js
@@ -17,6 +17,7 @@ const showProducts = (products) => {
     <img class="product-image" src=${image}></img>
       </div>
       <h3>${product.title}</h3>
+      <p class="text-muted"><i class="fas fa-star"></i> ${product.rating.rate} (${product.rating.count})</p>
       <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">Add to cart</button>
